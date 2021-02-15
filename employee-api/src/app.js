d6cceea3754b97d3c19127620dcb24e3274b47f2
dauryellen <dauryellen@live.com>
2021-02-15 08:56:13 -0300

    concluindo configurações no app.js

--- employee-api/src/app.js
+++ employee-api/src/app.js
@@ -9,6 +9,14 @@ const app = express();
 
 // rotas da api
 const index = require("./routes/index");
-const employeeRoute = require("./routes/employee.routes");
+//const employeeRoute = require("./routes/employee.routes");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json({ type: "application/vnd.api+json" }));
+app.use(cors());
+
+app.use(index);
+//app.use("/api/", employeeRoute);
+
+module.exports = app;
