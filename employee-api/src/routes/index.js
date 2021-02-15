e48455b4c2f97debda9c40cdd3a43cba04e934d6
dauryellen <dauryellen@live.com>
2021-02-15 09:01:57 -0300

    criando primeira rota da aplicação

---
+++ employee-api/src/routes/index.js
@@ -0,0 +1,17 @@
+/**
+ * arquivo responsável pela chamada da api na aplicação no lado do back-end
+ */
+
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/api", (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: "Seja bem-vindo(a) a API Node.js + PostgreSQL + Azure",
+    version: "1.0.0",
+  });
+});
+
+module.exports = router;
