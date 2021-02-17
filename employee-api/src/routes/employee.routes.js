4990d67617a7860e8ec144acd16bc74cbd82266f
dauryellen <dauryellen@live.com>
2021-02-17 19:37:25 -0300

    criação da rota de listagem

--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -5,7 +5,10 @@
 const router = require("express-promise-router")();
 const employeeController = require("../controllers/employee.controller");
 
-// rota responsável por criar novo funcionário(a):(POST):localhost:3000/api/employess
+// rota responsável por criar novo funcionário(a):(POST):localhost:3000/api/employees
 router.post("/employees", employeeController.createEmployee);
+
+// rota responsável por listar todos os funcionários(as):(GET):localhost:3000/api/employees
+router.get("/employees", employeeController.listAllEmployees);
 
 module.exports = router;
