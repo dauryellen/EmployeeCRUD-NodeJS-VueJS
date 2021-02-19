895e4321465f00a400715e7c72aaa63c3619af9d
dauryellen <dauryellen@live.com>
2021-02-19 10:21:14 -0300

    componentes do crud

---
+++ front-vue-v2/src/components/pages/edit-employee/EditEmployeeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row justify-content-center">
+    <h1>Edit Employee</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditEmployeeComponent",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
