24262db01dc8f156a47a530b72c5f859f5de95d6
dauryellen <dauryellen@live.com>
2021-02-24 21:57:27 -0300

    criando as rotas do app

--- front-vue-v2/src/router/index.js
+++ front-vue-v2/src/router/index.js
@@ -1,29 +1,30 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: "/",
+    name: "Create New Employee",
+    component: () => import("../components/pages/create-employee/CreateEmployeeComponent")
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: "/list-employees",
+    name: "List Employees",
+    component: () => import("../components/pages/list-employee/ListEmployeeComponent")
   },
+  {
+    path: "/edit-employee/:id",
+    name: "Update Employee",
+    component: () => import("../components/pages/edit-employee/EditEmployeeComponent")
+  }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
